46c4a40912536db469f1b9466fb11ad5da34fccc
lamprinikourou <lamprinikourou@gmail.com>
2024-12-16 22:57:05 +0200

    Fix or update logic in serializers.py

--- api/serializers.py
+++ api/serializers.py
@@ -1,16 +1,21 @@
 from rest_framework import serializers
 from .models import User, Product, Order
-
+from django.utils.safestring import mark_safe
+ 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = '__all__'
-
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        representation['username'] = mark_safe(instance.username)
+        return representation
+ 
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = '__all__'
-
+ 
 class OrderSerializer(serializers.ModelSerializer):
     class Meta:
         model = Order
